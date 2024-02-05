418f80a8a6b54e5f221b5619cd997a4c1865caae
Frederick Asante <Rastinga1995@hotmail.com>
2024-02-05 05:34:31 +0000

    Add project files.

---
+++ DemoClipboardInBlazorWebApp/Program.cs
@@ -0,0 +1,31 @@
+using DemoClipboardInBlazorWebApp.Components;
+using NetcodeHub.Packages.Components.Toast;
+using NetcodeHub.Packages.Extensions.Clipboard;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddScoped<ClipboardService>();
+builder.Services.AddScoped<ToastService>();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
